82a17976f155ab36225484e3434b527b115e10d0
xgpxg <32010816+xgpxg@users.noreply.github.com>
2026-02-25 16:39:27 +0800

    Change static CRT setting from true to false

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag("-std=c++11")
-        .static_crt(true)
+        .static_crt(false)
         .file("src/esaxx.cpp")
         .include("src")
         .compile("esaxx");
